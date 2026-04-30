17f16986ba9d67fb551cdb21c73523310b188472
niharika2k00 <dniharika16@gmail.com>
2026-05-01 00:29:19 +0530

    update repo base

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const isGithubPages = process.env.DEPLOY_TARGET === "github";
-const repoBase = "/Portfolio";
+const repoBase = "/portfolio";
 
 const nextConfig: NextConfig = {
   output: "export",
